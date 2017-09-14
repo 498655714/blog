cd9b4a9153df7655f8277874343d65ec336884cb
nealjiao <498655714@qq.com>
2017-09-14 12:55:43 +0800

    cieshaidsa f

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::get('/', function () {
 });
 
 Route::get('hello',function(){
-    return 'Hello, welcome to git push upadte toooooooooooo';
+    return 'Hello, welcome to git push upadte toooosdsdsoooooooo';
 });
