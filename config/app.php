b77fa4a3e493327be7bf49d8febcad423257f771
498655714 <498655714@qq.com>
2017-09-14 16:07:28 +0800

    设.env

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:dNny6jocK0P64zY4sxDyGo9FGNusQPSQKBF7gjfQOqo='),
 
     'cipher' => 'AES-256-CBC',
 
