d8a42b83fd96701939e4a9fd41dda743f24bfa02
498655714 <498655714@qq.com>
2017-09-15 16:28:42 +0800

    qq

---
+++ app/Model/PasswordResets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+class PasswordResets extends Model
+{
+    protected $tabel='password_resets';
+    protected $primaryKey='email';
+    public $timstamps=false;
+}
