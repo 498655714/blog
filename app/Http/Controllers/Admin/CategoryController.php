2db6bbe51d9a11c2014f41a00e46f2254ba64547
498655714 <498655714@qq.com>
2017-09-18 15:07:39 +0800

    dads

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -31,20 +31,21 @@ class CategoryController extends CommonController{
     }
     public function vieworder(Request $request){
         $info = $request->all();
-        $category = new Category();
-        $res = $category->where(['cate_id'=>$info['cate_id']])->update(['cate_order'=>$info['cate_order']]);
-        if($res){
-            $data =[
-                'status'=>1,
-                'msg'=>'文章分类排序修改成功！'
-            ];
-        }else{
-            $data =[
-                'status'=>2,
-                'msg'=>'文章分类排序修改失败！'
-            ];
-        }
-        return $data;
+        dd($info);
+//        $category = new Category();
+//        $res = $category->where(['cate_id'=>$info['cate_id']])->update(['cate_order'=>$info['cate_order']]);
+//        if($res){
+//            $data =[
+//                'status'=>1,
+//                'msg'=>'文章分类排序修改成功！'
+//            ];
+//        }else{
+//            $data =[
+//                'status'=>2,
+//                'msg'=>'文章分类排序修改失败！'
+//            ];
+//        }
+//        return $data;
 
     }
     //全部分类列表
