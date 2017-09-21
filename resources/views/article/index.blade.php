dd2d8af93ce86dcb679d689b74f9ef847c7141fe
498655714 <498655714@qq.com>
2017-09-21 20:02:09 +0800

    dsads

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -63,7 +63,7 @@
                                 <td class="hidden-480 ">{{$val['art_description']}}</td>
                                 <td class=" ">
                                     @if(isset($val['art_tag']) && !empty($val['art_tag']))
-                                        {{$tag = explode(',',$val['art_tag'])}}
+                                        <?php $tag = explode(',',$val['art_tag']) ?>
                                         @foreach($tag as $tag_id)
                                             @foreach($tags as $tag_key => $tag_val)
                                                 @if($tag_key == $tag_id)
