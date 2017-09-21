6f4d58c2b381419d1ac01b53a711aab4ea92375e
498655714 <498655714@qq.com>
2017-09-21 18:43:58 +0800

    dad

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -64,7 +64,15 @@
             <div class="col-sm-6">
                 @foreach($tags as $tag=>$value)
                     <label>
-                        <input name="art_tag[]" class="ace" type="checkbox" @if(isset($data[0]['art_tag']) && !empty($data[0]['art_tag'])) @foreach($data[0]['art_tag'] as $tag) @if($tag == $value['tag_id'])) checked @endif @endforeach  @endif value="{{$value['tag_id']}}">
+                        <input name="art_tag[]" class="ace" type="checkbox"
+                               @if(isset($data[0]['art_tag']) && !empty($data[0]['art_tag']))
+                                   @foreach($data[0]['art_tag'] as $tag)
+                                        @if($tag == $value['tag_id'])
+                                            checked
+                                        @endif
+                                   @endforeach
+                               @endif
+                               value="{{$value['tag_id']}}">
                         <span class="lbl"> {{$value['tag_name']}}&nbsp;&nbsp;&nbsp;</span>
                     </label>
                     @if((!($key+1)%8))
