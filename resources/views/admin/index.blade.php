9ea3402ae344ebffc932cfdb8659c0c592c173e3
498655714 <498655714@qq.com>
2017-09-22 12:23:25 +0800

    dasd

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -589,9 +589,41 @@
         });
     };
     jQuery(function($) {
+        var animationSpeed = 300;
+        $('ul[class="nav nav-list"]').on('click', 'li a', function(e) {
+            var $this = $(this);
+            var checkElement = $this.next();
 
-        $('.nav .nav-list').on('click', 'li a', function(e) {
-            alert();
+            if (checkElement.is('.submenu') && checkElement.is(':visible')) {
+                checkElement.slideUp(animationSpeed, function() {
+                    checkElement.removeClass('open');
+                });
+                checkElement.parent("li").removeClass("active");
+            }
+
+            //If the menu is not visible
+            else if ((checkElement.is('.submenu')) && (!checkElement.is(':visible'))) {
+                //Get the parent menu
+                var parent = $this.parents('ul').first();
+                //Close all open menus within the parent
+                var ul = parent.find('ul:visible').slideUp(animationSpeed);
+                //Remove the menu-open class from the parent
+                ul.removeClass('open');
+                //Get the parent li
+                var parent_li = $this.parent("li");
+
+                //Open the target menu and add the menu-open class
+                checkElement.slideDown(animationSpeed, function() {
+                    //Add the class active to the parent li
+                    checkElement.addClass('open');
+                    parent.find('li.active').removeClass('active');
+                    parent_li.addClass('active');
+                });
+            }
+            //if this isn't a link, prevent the page from being redirected
+            if (checkElement.is('.submenu')) {
+                e.preventDefault();
+            }
         });
 
         $('.easy-pie-chart.percentage').each(function(){
