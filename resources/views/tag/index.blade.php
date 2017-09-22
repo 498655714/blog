151d1ce0ffdedccacd288cec2510c2e5013464b5
498655714 <498655714@qq.com>
2017-09-22 15:09:30 +0800

    dasd

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>标签页面</title>
+    <link rel="stylesheet" href="{{asset('assets/css/ace.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/ace-rtl.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/ace-skins.min.css')}}" />
     <script src="{{asset('assets/js/jquery-2.0.3.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/ace-elements.min.js')}}"></script>
+    <script src="{{asset('assets/js/ace.min.js')}}"></script>
     <script type="text/javascript">
         $(function () {
         });
     </script>
 </head>
 <body>
-<div  id="CloudLabel">
+<div >
     <?php $data = [
             'label',
             'label label-success arrowed',
@@ -23,6 +28,9 @@
     @for($i=0;$i<count($tags);$i++)
             <span class="{{$data[$i]}}">{{$val['tag_name']}}</span>&nbsp;
     @endfor
+</div>
+<div>
+    <a href="#">添加标签</a>&nbsp;&nbsp;&nbsp;<a href="#">删除标签</a>
 </div>
 </body>
 </html>
