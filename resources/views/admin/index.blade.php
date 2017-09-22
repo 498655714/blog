6be7ed144935443472b34349afa3b1bb36f69be3
498655714 <498655714@qq.com>
2017-09-22 13:42:14 +0800

    sdafdsa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -593,16 +593,38 @@
         $('ul[class="nav nav-list"]').on('click', 'li a', function(e) {
             var $this = $(this);
             var checkElement = $this.next();
-            $('ul[class="nav nav-list"] li').removeClass('active').removeClass('open');
-            $(this).addClass('active');
-            if (checkElement.is('.submenu')) {
-                checkElement.parent("li").addClass("open");
-                //checkElement.addClass(active);
+
+            if (checkElement.is('.submenu') && checkElement.is(':visible')) {
+                checkElement.slideUp(animationSpeed, function() {
+                    checkElement.removeClass('open');
+                });
+                checkElement.parent("li").removeClass("active");
             }
 
-            if( $(this).parents('ul').is('.submenu')){
-                $(this).parents('ul').parent("li").addClass("active open");
+            //If the menu is not visible
+            else if ((checkElement.is('.submenu')) && (!checkElement.is(':visible'))) {
+                //Get the parent menu
+                var parent = $this.parents('ul').first();
+                //Close all open menus within the parent
+                var ul = parent.find('ul:visible').slideUp(animationSpeed);
+                //Remove the menu-open class from the parent
+                ul.removeClass('open');
+                //Get the parent li
+                var parent_li = $this.parent("li");
+
+                //Open the target menu and add the menu-open class
+                checkElement.slideDown(animationSpeed, function() {
+                    //Add the class active to the parent li
+                    checkElement.addClass('open');
+                    parent.find('li.active').removeClass('active');
+                    parent_li.addClass('active');
+                });
             }
+            //if this isn't a link, prevent the page from being redirected
+            if (checkElement.is('.submenu')) {
+                e.preventDefault();
+            }
+
 
         });
 
