ccfbbc776d359d43c67228d2c403820abb33e042
498655714 <498655714@qq.com>
2017-09-24 02:41:42 +0800

    add 404 error

---
+++ resources/views/main/404page.blade.php
@@ -0,0 +1,15 @@
+﻿<!doctype html>
+<html style="background: #fff;">
+<head>
+	<meta charset="utf-8">
+	<title>404错误</title>
+	<link rel="stylesheet" type="text/css" href="{{url('main/css/style.css')}}" />
+</head>
+<body>
+	<div class="notice_page">
+		<div class="notice_404_p"></div>
+		<div class="home_page"><a class="notice_retun" href="javascript:history.go(-1);">返回上一页</a><a class="notice_home" href="url('main/index')">返回首页</a></div>
+	</div>
+
+</body>
+</html>
