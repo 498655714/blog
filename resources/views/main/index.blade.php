e09c60cf8fe99c4fd40b9f7b8c321e9a723cc31d
498655714 <498655714@qq.com>
2017-09-23 20:42:05 +0800

    dasd

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -70,11 +70,14 @@
                 <footer class="article-footer">
                     <div class="article-footer-l">
                         <i class="iconfont icon-tags"></i>
-                        @foreach($art_val['art_tag'] as $tag_key=>$tag_val)
-                            &nbsp;&nbsp;<a class="article-tag" data-toggle="tooltip" data-placement="top" title="bootstrap">
-                                {{$tag_val['tag_name']}}
-                            </a>&nbsp;&nbsp;
-                        @endforeach
+                        @if(!empty($art_val['art_tag']) && is_array($art_val['art_tag']))
+                            @foreach($art_val['art_tag'] as $tag_key=>$tag_val)
+                                &nbsp;&nbsp;<a class="article-tag" data-toggle="tooltip" data-placement="top"
+                                               title="bootstrap">
+                                    {{$tag_val['tag_name']}}
+                                </a>&nbsp;&nbsp;
+                            @endforeach
+                        @endif
                     </div>
                     <div class="article-footer-r">
                         <i class="iconfont icon-hit"></i> {{$art_val['art_view']}}&nbsp;&nbsp;
